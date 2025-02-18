b3102c46f0f314f8338f880e4a8ef26943cd6537
xMurungi <115868141+xMurungi@users.noreply.github.com>
2025-02-19 00:46:10 +0300

    Add landlord details UI. Modify card shape. Add apartment details

--- My Apps/Mali/composeApp/src/commonMain/kotlin/com/joses/mali/ui/LandlordsScreen.kt
+++ My Apps/Mali/composeApp/src/commonMain/kotlin/com/joses/mali/ui/LandlordsScreen.kt
@@ -1,11 +1,18 @@
 package com.joses.mali.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -14,6 +21,7 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.serialization.Serializable
@@ -50,6 +58,13 @@ fun LandlordView() {
        val padd = it
 
         LazyColumn {
+            item {
+                LandLordDetails(
+                    modifier = Modifier
+                        .padding(20.dp)
+                )
+            }
+
             items(mastuff) { data ->
                 DataView(
                     stuff = data
@@ -61,13 +76,59 @@ fun LandlordView() {
 }
 
 @Composable
+fun LandLordDetails(
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(20.dp),
+        elevation = 10.dp
+    ) {
+        Row(
+            modifier = modifier
+        ) {
+            Column {
+                Image(
+                    painter = painterResource(Res.drawable.Aptmnt_Image),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                )
+                Text(
+                    text = "John Doe"
+                )
+            }
+            Column(
+                modifier = modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Row {
+                    Text(
+                        text = "Properties: "
+                    )
+                    Text(
+                        text = "${mastuff.size}"
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
 fun DataView(
    stuff: String
 ) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(50.dp)
+            .padding(50.dp),
+        shape = RoundedCornerShape(20.dp),
     ) {
         Column {
             Image(
@@ -76,18 +137,38 @@ fun DataView(
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally),
             )
-            Text(
-                text = "Amani Apartment"
-            )
-            Text(
-                text = "14 Units"
-            )
-            Text(
-                text = "14 Tenants"
-            )
-            Text(
-                text = stuff
-            )
+            Column(
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Text(
+                    text = "Amani Apartment"
+                )
+                Text(
+                    text = "14 Units"
+                )
+                Text(
+                    text = "14 Tenants"
+                )
+                Row {
+                    Text(
+                        text = "PayBill: "
+                    )
+                    Text(
+                        text = "247247"
+                    )
+                }
+                Row {
+                    Text(
+                        text = "Accnt. No.: "
+                    )
+                    Text(
+                        text = "221053"
+                    )
+                }
+                Text(
+                    text = stuff
+                )
+            }
         }
     }
 }
