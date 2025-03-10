e69161091457612d8c507427afcf3de9b2c044be
xMurungi <115868141+xMurungi@users.noreply.github.com>
2025-03-10 20:46:31 +0300

    Initialize Daraja client on iOS app

--- My Apps/Mali/composeApp/src/iosMain/kotlin/com/joses/mali/MainViewController.kt
+++ My Apps/Mali/composeApp/src/iosMain/kotlin/com/joses/mali/MainViewController.kt
@@ -1,5 +1,19 @@
 package com.joses.mali
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import com.joses.mali.tenants.network.MpesaStkPushClient
+import com.joses.mali.tenants.network.createHttpClient
+import io.ktor.client.engine.darwin.Darwin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App(
+        mpesaClient = remember {
+            MpesaStkPushClient(
+                createHttpClient(
+                    Darwin.create()
+                )
+            )
+        }
+    )
+}
