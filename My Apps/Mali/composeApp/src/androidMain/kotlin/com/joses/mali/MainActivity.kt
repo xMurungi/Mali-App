16e4223848348775f1cea5d7bb545fe5f500fd84
xMurungi <115868141+xMurungi@users.noreply.github.com>
2025-03-31 18:04:55 +0300

    fix: update Android manifest and main activity

--- My Apps/Mali/composeApp/src/androidMain/kotlin/com/joses/mali/MainActivity.kt
+++ My Apps/Mali/composeApp/src/androidMain/kotlin/com/joses/mali/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.joses.mali
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.FirebaseApp
 import com.joses.mali.tenants.network.MpesaStkPushClient
 import com.joses.mali.tenants.network.createHttpClient
 import io.ktor.client.engine.okhttp.OkHttp
+import android.os.Environment
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,22 @@ class MainActivity : ComponentActivity() {
         FirebaseApp.initializeApp(this)
 
         setContent {
+            val context = LocalContext.current
+            val pdfGenerator = PdfGeneratorImpl()
+//            val filePath = "${context.filesDir}/sample.pdf"
+
+            val downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            val filePath = File(downloadsDir, "JohnPayments.pdf").absolutePath
+
+            val pdfAptGenerator = PdfApartmentGeneratorImpl()
+            val aptFilePath = File(downloadsDir, "UmojaApartment.pdf").absolutePath
+
+            val pdfTenantGenerator = PastTenantPdfGeneratorImpl()
+            val tenantFilePath = File(downloadsDir, "PastTenants.pdf").absolutePath
+
+            val allPdfTenantGenerator = TenantPdfGenerator()
+            val allTenantFilePath = File(downloadsDir, "AllCurrentTenants.pdf").absolutePath
+
             App(
                 mpesaClient = remember {
                     MpesaStkPushClient(
@@ -24,6 +44,22 @@ class MainActivity : ComponentActivity() {
                             OkHttp.create()
                         )
                     )
+                },
+                onGeneratePdf = {
+                    pdfGenerator.generatePdf(filePath)
+                    Toast.makeText(this, "\uD83D\uDCC4 PDF Generated at $filePath", Toast.LENGTH_LONG).show()
+                },
+                generateApartmentPaymentReport = {
+                    pdfAptGenerator.generatePdf(aptFilePath)
+                    Toast.makeText(this, "\uD83D\uDCC4 PDF Generated at $aptFilePath", Toast.LENGTH_LONG).show()
+                },
+                onGeneratePastTenantPdf = {
+                    pdfTenantGenerator.generatePdf(tenantFilePath)
+                    Toast.makeText(this, "\uD83D\uDCC4 PDF Generated at $tenantFilePath", Toast.LENGTH_LONG).show()
+                },
+                generateTenantPdf = {
+                    allPdfTenantGenerator.generatePdf(allTenantFilePath)
+                    Toast.makeText(this, "\uD83D\uDCC4 PDF Generated at $allTenantFilePath", Toast.LENGTH_LONG).show()
                 }
             )
         }
@@ -33,6 +69,21 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
+    val context = LocalContext.current
+    val pdfGenerator = PdfGeneratorImpl()
+    val filePath = "${context.filesDir}/sample.pdf"
+
+    val downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+    val pdfAptGenerator = PdfGeneratorImpl()
+    val aptFilePath = File(downloadsDir, "UmojaApartment.pdf").absolutePath
+
+    val pdfTenantGenerator = PastTenantPdfGeneratorImpl()
+    val tenantFilePath = File(downloadsDir, "PastTenants.pdf").absolutePath
+
+    val allPdfTenantGenerator = PastTenantPdfGeneratorImpl()
+    val allTenantFilePath = File(downloadsDir, "PastTenants.pdf").absolutePath
+
     App(
         mpesaClient = remember {
             MpesaStkPushClient(
@@ -40,6 +91,22 @@ fun AppAndroidPreview() {
                     OkHttp.create()
                 )
             )
+        },
+        onGeneratePdf = {
+            pdfGenerator.generatePdf(filePath)
+//            Toast.makeText(this, "PDF Generated at $filePath", Toast.LENGTH_LONG).show()
+        },
+        generateApartmentPaymentReport = {
+            pdfAptGenerator.generatePdf(filePath)
+//            Toast.makeText(this, "\uD83D\uDCC4 PDF Generated at $aptFilePath", Toast.LENGTH_LONG).show()
+        },
+        onGeneratePastTenantPdf = {
+            pdfTenantGenerator.generatePdf(tenantFilePath)
+//            Toast.makeText(this, "\uD83D\uDCC4 PDF Generated at $tenantFilePath", Toast.LENGTH_LONG).show()
+        },
+        generateTenantPdf = {
+            allPdfTenantGenerator.generatePdf(allTenantFilePath)
+//            Toast.makeText(this, "\uD83D\uDCC4 PDF Generated at $allTenantFilePath", Toast.LENGTH_LONG).show()
         }
     )
 }
