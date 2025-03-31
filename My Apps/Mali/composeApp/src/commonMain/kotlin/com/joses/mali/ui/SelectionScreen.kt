f262b6cc9567b76f960382d4895d18d601ccd969
xMurungi <115868141+xMurungi@users.noreply.github.com>
2025-03-31 18:09:15 +0300

    Improve selection screen UI. Add users name to greeting message

--- My Apps/Mali/composeApp/src/commonMain/kotlin/com/joses/mali/ui/SelectionScreen.kt
+++ My Apps/Mali/composeApp/src/commonMain/kotlin/com/joses/mali/ui/SelectionScreen.kt
@@ -2,13 +2,11 @@ package com.joses.mali.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,15 +14,24 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.joses.mali.landlord.ui.LandlordsScreen
+import com.joses.mali.landlord.ui.LandlordsScreenRoute
 import com.joses.mali.tenants.ui.TenantsScreen
+import com.joses.mali.tenants.ui.TenantsScreen2
 import kotlinx.serialization.Serializable
 
 @Serializable
-object SelectionScreen
+data class SelectionScreen(
+    val id: String = "",
+    val name: String = "",
+    val profilePicUrl: String = ""
+)
 
 @Composable
 fun SelectionScreen(
-    navController: NavController
+    navController: NavController,
+    id: String = "",
+    name: String = "",
+    profilePicUrl: String = ""
 ) {
 
     Column(
@@ -38,25 +45,33 @@ fun SelectionScreen(
             modifier = Modifier
                 .padding(vertical = 50.dp)
                 .fillMaxWidth(),
-            backgroundColor = MaterialTheme.colors.background,
+//            backgroundColor = MaterialTheme.colors.background,
             shape = MaterialTheme.shapes.medium,
-            elevation = 50.dp
+            elevation = CardDefaults.cardElevation(defaultElevation = 50.dp)
         ) {
             Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .padding(horizontal = 20.dp, vertical = 50.dp)
+                    .fillMaxWidth()
             ) {
+
                 Text(
-                    text = "Welcome to Mali App",
+                    text = "Welcome to Mali App, $name",
                     fontSize = 25.sp
+                )
+
+                Spacer(
+                    modifier = Modifier.padding(8.dp)
                 )
 
                 Button(
                     onClick = {
                         navController.navigate(
-                            LandlordsScreen
+                            LandlordsScreenRoute(
+                                name = name
+                            )
                         )
                     },
                 ) {
@@ -68,7 +83,7 @@ fun SelectionScreen(
                 Button(
                     onClick = {
                         navController.navigate(
-                            TenantsScreen
+                            TenantsScreen2
                         )
                     },
                 ) {
