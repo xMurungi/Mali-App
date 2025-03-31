5baff9020c35adbca556e23f98902fbae5a089ac
xMurungi <115868141+xMurungi@users.noreply.github.com>
2025-03-31 18:04:40 +0300

    chore: update main app structure and navigation

--- My Apps/Mali/composeApp/src/commonMain/kotlin/com/joses/mali/App.kt
+++ My Apps/Mali/composeApp/src/commonMain/kotlin/com/joses/mali/App.kt
@@ -1,16 +1,43 @@
 package com.joses.mali
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.joses.mali.navigation.MaliApp
 import com.joses.mali.tenants.network.MpesaStkPushClient
+import com.joses.mali.tenants.repository.FirestoreUserRepository
+import com.mmk.kmpauth.google.GoogleAuthCredentials
+import com.mmk.kmpauth.google.GoogleAuthProvider
+import com.mmk.kmpauth.google.GoogleButtonUiContainer
+import com.mmk.kmpauth.uihelper.google.GoogleSignInButton
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App(
     mpesaClient: MpesaStkPushClient,
+    onGeneratePdf: () -> Unit,
+    generateApartmentPaymentReport: () -> Unit,
+    onGeneratePastTenantPdf: () -> Unit,
+    generateTenantPdf: () -> Unit
 ) {
+
+    val userRepository = remember { FirestoreUserRepository() }
+
     MaliApp(
-        mpesaStkPushClient = mpesaClient
+        mpesaStkPushClient = mpesaClient,
+        userRepository = userRepository,
+        onGeneratePdf = onGeneratePdf,
+        generateApartmentPaymentReport = generateApartmentPaymentReport,
+        onGeneratePastTenantPdf = onGeneratePastTenantPdf,
+        generateTenantPdf = generateTenantPdf
     )
+
 }
--- My Apps/Mali/composeApp/src/commonMain/kotlin/com/joses/mali/navigation/MaliApp.kt
+++ My Apps/Mali/composeApp/src/commonMain/kotlin/com/joses/mali/navigation/MaliApp.kt
@@ -1,48 +1,174 @@
 package com.joses.mali.navigation
 
 import androidx.compose.foundation.background
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.joses.mali.landlord.repository.LandlordRepository
+import com.joses.mali.landlord.ui.AllTenants
+import com.joses.mali.landlord.ui.AllTenantsRoute
+import com.joses.mali.landlord.ui.ApartmentPayments
+import com.joses.mali.landlord.ui.ApartmentPaymentsRoute
 import com.joses.mali.landlord.ui.LandlordsScreen
+import com.joses.mali.landlord.ui.LandlordsScreenRoute
+import com.joses.mali.landlord.ui.PastTenants
+import com.joses.mali.landlord.ui.PastTenantsRoute
+import com.joses.mali.landlord.ui.UnitPaymentsHistory
+import com.joses.mali.landlord.ui.UnitPaymentsHistoryUi
+import com.joses.mali.tenants.domain.UserRepository
 import com.joses.mali.tenants.network.MpesaStkPushClient
+import com.joses.mali.tenants.ui.LoginPage
+import com.joses.mali.tenants.ui.LoginPageUi
+import com.joses.mali.tenants.ui.PaymentHistory
+import com.joses.mali.tenants.ui.PaymentHistoryUi
+import com.joses.mali.tenants.ui.RentStatus
+import com.joses.mali.tenants.ui.RentStatusUi
+import com.joses.mali.tenants.ui.TenantSearchScreen
 import com.joses.mali.ui.SelectionScreen
 import com.joses.mali.tenants.ui.TenantsScreen
+import com.joses.mali.tenants.ui.TenantsScreen2
+import com.joses.mali.tenants.ui.UpcomingBill
+import com.joses.mali.tenants.ui.UpcomingBillUi
 
 @Composable
 fun MaliApp(
-    mpesaStkPushClient: MpesaStkPushClient
+    mpesaStkPushClient: MpesaStkPushClient,
+    userRepository: UserRepository,
+    onGeneratePdf: () -> Unit,
+    generateApartmentPaymentReport: () -> Unit,
+    onGeneratePastTenantPdf: () -> Unit,
+    generateTenantPdf: () -> Unit
 ) {
 
     val navController = rememberNavController()
-    val startDestinationString = SelectionScreen
+    val startDestinationString = LoginPageUi
+    val landlordRepository = remember { LandlordRepository() }
 
     NavHost(
         navController = navController,
         startDestination = startDestinationString,
         Modifier
-            .background(MaterialTheme.colors.background)
+            .background(MaterialTheme.colorScheme.background)
     ) {
 
-        composable<SelectionScreen> {
-            SelectionScreen(
+        composable<LoginPageUi> {
+            LoginPage(
                 navController = navController
             )
         }
 
-        composable<LandlordsScreen> {
+        composable<SelectionScreen> {
+            val args = it.toRoute<SelectionScreen>()
+            SelectionScreen(
+                navController = navController,
+                id = args.id,
+                name = args.name,
+                profilePicUrl = args.profilePicUrl,
+            )
+        }
+
+        composable<LandlordsScreenRoute> {
+            val args = it.toRoute<LandlordsScreenRoute>()
             LandlordsScreen(
+                navController = navController,
+                name = args.name
+            )
+        }
+
+        composable<AllTenantsRoute> {
+            val args = it.toRoute<AllTenantsRoute>()
+            AllTenants(
+                navController = navController,
+                apartmentId = args.apartmentId,
+                generateTenantPdf = { generateTenantPdf() }
+            )
+        }
+
+        composable<ApartmentPaymentsRoute> {
+            val args = it.toRoute<ApartmentPaymentsRoute>()
+            ApartmentPayments(
+                navController = navController,
+                apartmentId = args.apartmentId,
+                generateApartmentPaymentReport = { generateApartmentPaymentReport() }
+            )
+        }
+
+        composable<PastTenantsRoute> {
+            val args = it.toRoute<PastTenantsRoute>()
+            PastTenants(
+                navController = navController,
+                apartmentId = args.apartmentId,
+                onGeneratePastTenantPdf = { onGeneratePastTenantPdf() }
+            )
+        }
+
+        composable<UnitPaymentsHistoryUi> {
+            val args = it.toRoute<UnitPaymentsHistoryUi>()
+            UnitPaymentsHistory(
+                navController = navController,
+                tenantId = args.tenantId
+            )
+        }
+
+        composable<TenantsScreen2> {
+            TenantSearchScreen(
+                apartmentId = "",
+                landlordRepository = landlordRepository,
                 navController = navController
             )
+//            TenantsScreen(
+//                navController = navController,
+//                mpesaStkPushClient = mpesaStkPushClient,
+//                repository = userRepository,
+//                onGeneratePdf = { onGeneratePdf() }
+//            )
         }
 
         composable<TenantsScreen> {
+//            TenantSearchScreen(
+//                apartmentId = "",
+//                landlordRepository = landlordRepository,
+//                onTenantSelected = {
+//                    navController.navigate(
+//                        route = TenantsScreen
+//                    )
+//                },
+//            )
+
             TenantsScreen(
                 navController = navController,
-                mpesaStkPushClient = mpesaStkPushClient
+                mpesaStkPushClient = mpesaStkPushClient,
+                repository = userRepository,
+                onGeneratePdf = { onGeneratePdf() }
+            )
+        }
+
+        composable<PaymentHistoryUi> {
+            val args = it.toRoute<PaymentHistoryUi>()
+            PaymentHistory(
+                navController = navController,
+                tenantId = args.tenantId
+            )
+        }
+
+        composable<RentStatusUi> {
+            val args = it.toRoute<RentStatusUi>()
+            RentStatus(
+                navController = navController,
+                tenantId = args.tenantId
+            )
+        }
+
+        composable<UpcomingBillUi> {
+            val args = it.toRoute<UpcomingBillUi>()
+            UpcomingBill(
+                navController = navController,
+                tenantId = args.tenantId
             )
         }
 
